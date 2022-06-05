0062fa159233839da90d78a1a59cc14cadc3bf0b
Code Melody <lhr626333250@hotmail.com>
2022-06-05 23:33:17 +0800

    创建WinUI3项目

---
+++ Misaka.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Misaka.WinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
