0b02188a36eee50a9e42d05ec2f68e21f01cbbd1
Code Melody <lhr626333250@hotmail.com>
2022-07-17 14:29:01 +0800

    Rebuild the settings of CodePage with Json

---
+++ Misaka.AppEnv/SettingsCodepage.cpp
@@ -0,0 +1,20 @@
+﻿#include "Percompilation.h"
+#include "SettingsCodepage.h"
+#if __has_include("SettingsCodepage.g.cpp")
+#include "SettingsCodepage.g.cpp"
+#endif
+
+namespace winrt::Misaka::AppEnv::implementation
+{
+    SettingsCodepage::SettingsCodepage() : base_type(L"CodePage") { ; }
+
+    hstring SettingsCodepage::LocaleEmulatorPath()
+    {
+        return this->GetValue(L"LocaleEmulatorPath", hstring());
+    }
+
+    void SettingsCodepage::LocaleEmulatorPath(hstring value)
+    {
+        this->SetValue(L"LocaleEmulatorPath", value);
+    }
+}
---
+++ Misaka.AppEnv/SettingsCodepage.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "SettingsCodepage.g.h"
+#include "SettingsBase.h"
+
+namespace winrt::Misaka::AppEnv::implementation
+{
+    struct SettingsCodepage : SettingsCodepageT<SettingsCodepage, implementation::SettingsBase>
+    {
+    public:
+        SettingsCodepage();
+        hstring LocaleEmulatorPath();
+        void LocaleEmulatorPath(hstring value);
+    };
+}
+
+namespace winrt::Misaka::AppEnv::factory_implementation
+{
+    struct SettingsCodepage : SettingsCodepageT<SettingsCodepage, implementation::SettingsCodepage> {};
+}
