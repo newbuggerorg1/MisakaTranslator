a414943ae7e8e6875e859f2dbf5bfed10a71688f
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-20 13:51:20 +0800

    Create DangoOCR.cs

---
+++ OCRLibrary/DangoOCR.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+
+namespace OCRLibrary
+{
+    public class DangoOCR : OCREngine
+    {
+        public string srcLangCode;
+        // private OcrEngine dangoOcr;
+
+        public override async Task<string> OCRProcessAsync(Bitmap img)
+        {
+            try
+            {
+                using var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+                img.Save(stream.AsStream(), ImageFormat.Bmp);
+                var decoder = await BitmapDecoder.CreateAsync(stream);
+                var bitmap = await decoder.GetSoftwareBitmapAsync();
+                var res = await dangoOcr.RecognizeAsync(bitmap);
+                return Task.FromResult(res.Text).Result;
+            }
+            catch (Exception ex)
+            {
+                errorInfo = ex.Message;
+                return string.Empty;
+            }
+        }
+
+        public override bool OCR_Init(string param1 = "", string param2 = "")
+        {
+            try
+            {
+                Language lang = new(srcLangCode);
+                dangoOcr = OcrEngine.TryCreateFromLanguage(lang);
+                if (dangoOcr == null)
+                {
+                    System.Windows.MessageBox.Show($"请在Windows设置App中添加OCR组件。{System.Environment.NewLine}Please install OCR component in Windows Settings App.");
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errorInfo = ex.Message;
+                return false;
+            }
+        }
+
+        public override void SetOCRSourceLang(string lang)
+        {
+            if (lang == "jpn")
+            {
+                srcLangCode = "ja-jp";
+            }
+            else if (lang == "eng")
+            {
+                srcLangCode = "en-us";
+            }
+        }
+
+        public IReadOnlyList<Language> GetSupportLang()
+        {
+            return OcrEngine.AvailableRecognizerLanguages;
+        }
+    }
+}
